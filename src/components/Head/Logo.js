8b9654c596bc6475860715df1f59655f013c5b8a
BangDori <qudwns8865@naver.com>
2023-01-08 23:26:19 +0900

    Feat: title logo

---
+++ src/components/Head/Logo.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+export default class Logo extends Component {
+    shouldComponentUpdate() {
+        return false;
+    }
+
+    render() {
+        console.log("Logo Rendering");
+
+        return(
+            
+            <div className="logo">
+                <img src="/images/logo.png" alt="Bangdori Cinema logo" />
+                뱅도리 시네마
+            </div>
+        )
+    }
+}
