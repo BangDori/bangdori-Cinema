3a1fe463bacfb0506ef1a986cf0c5c496c6f8807
BangDori <qudwns8865@naver.com>
2023-01-08 23:27:21 +0900

    Feat: foot

---
+++ src/components/Foot.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class Head extends Component {
+    shouldComponentUpdate() {
+        return true;
+    }
+
+    render() {
+        return(
+            <footer>
+                footer
+            </footer>
+        )
+    }
+}
