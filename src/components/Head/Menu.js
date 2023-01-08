c0dd134b0c9e7ffe72c90723d830802e271dd7e2
BangDori <qudwns8865@naver.com>
2023-01-09 01:42:14 +0900

    Fix: change stateName modal to menu

--- src/components/Head/Menu.js
+++ src/components/Head/Menu.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 export default class Menu extends Component {
     shouldComponentUpdate(newProps) {
-        if(this.props.modal === newProps.modal) {
+        if(this.props.menu === newProps.menu) {
             return false;
         }
 
@@ -17,10 +17,10 @@ export default class Menu extends Component {
                 <img id="decoMenu" src="/images/tree.png" alt="Menu decoration"></img>
                 <img id="menu" src="/images/menu.png" alt="Menu"
                     onClick={function(e){
-                        if(this.props.modal === 'on') {
-                            this.props.onModal('off');
+                        if(this.props.menu === 'on') {
+                            this.props.onMenu('off');
                         } else {
-                            this.props.onModal('on');
+                            this.props.onMenu('on');
                         }
                     }.bind(this)}
                 />
