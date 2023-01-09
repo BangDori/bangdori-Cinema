bc3b3b78e8ded6992f45b7ce83e9d4e2441f9e6c
BangDori <qudwns8865@naver.com>
2023-01-09 22:40:55 +0900

    Feat: navigation & content component

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
+import Navigation from './Body/Navigation';
+import Content from './Body/Content';
+import '../navigation.css';
+import '../content.css';
 
 export default class Body extends Component {
-    shouldComponentUpdate() {
+    shouldComponentUpdate(newProps) {
+        if(this.props.section === newProps.section) {
+            return false;
+        }
+
         return true;
     }
     
     render() {
+        console.log("Body render");
+
         return(
             <article>
-                Body
+                <Navigation
+                    section={this.props.section}
+                    button={this.props.button}
+                    onChangeSection={function(_section){
+                        this.props.onChangeSection(_section);
+                    }.bind(this)}
+                ></Navigation>
+
+                <Content
+                    topic={this.props.topic}
+                    content={this.props.content}
+                ></Content>
             </article>
         )
     }
