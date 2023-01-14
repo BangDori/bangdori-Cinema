9267ac601ca0390d42c4827b9fa9758e7c195bc9
BangDori <qudwns8865@naver.com>
2023-01-14 18:40:09 +0900

    Add: ShouldComponentUpdate function

--- src/components/Head.js
+++ src/components/Head.js
@@ -6,7 +6,8 @@ import '../assets/Head.css';
 
 export default class Head extends Component {
     shouldComponentUpdate(newProps, newState) {
-        if(this.props.state === newState) {
+        if((this.props.search === newProps.search)
+            && (this.props.menu === newProps.menu)) {
             return false;
         }
 
@@ -14,8 +15,6 @@ export default class Head extends Component {
     }
 
     render() {
-        // console.log("Head Rendering");
-
         return(
             <header>
                 <Logo></Logo>
